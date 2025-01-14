a177899d175d4b4bc159d5828d97eb72ec4c100a
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-14 02:40:31 +0100

    Update

--- themes/default/views/legalnotice/index.blade.php
+++ themes/default/views/legalnotice/index.blade.php
@@ -1,60 +1,57 @@
-<x-app-layout title="{{ __('Media') }}">
+<x-app-layout title="{{ __('Legal notice') }}">
 
     <div class="content">
         <div class="content-box">
             <div class="prose dark:prose-invert min-w-full">
                 <header class="flex flex-row gap-4">
-                    <h2 class="font-semibold text-2xl mb-2 mt-2 text-secondary-900">{{ __('Media') }}</h2>
+                    <h2 class="font-semibold text-2xl mb-2 mt-2 text-secondary-900">{{ __('Legal notice') }}</h2>
                 </header>
 
-                <p>
-                    Ihr habe Gefallen an dem was wir machen? Ihr habt einen Gameserver von mygserv.de oder einen
-                    Webspace? Dann habt ihr hier die Möglichkeit
-                    uns zu danken in dem ihr unseren Werbebanner auf eurer Homepage / Discord / Server platziert.
-                </p>
+                Angaben gemäß § 5 TMG:
 
-                <div class="mt-6 grid grid-cols-12 gap-4">
+                [Name des Anbieters][Adresse des Anbieters][PLZ Ort]
 
-                    <div class="lg:col-span-6 col-span-12 flex-col">
-                        <div class="">
-                            <textarea
-                                class="resize-none rounded-md w-full bg-secondary-50 text-gray-400"><a title="mygserv.de" href="https://mygserv.de"><img src="https://media.mygserv.de/banner/banner-468-60-blue-black.png" alt="mygserv.de" /></a></textarea>
-                        </div>
-                    </div>
-                    <div class="lg:col-span-6 col-span-12">
-                        <div class="flex justify-center items-center">
-                            <img class="inline-block align-middle mt-0 mb-0 center"
-                                src="https://media.mygserv.de/banner/banner-468-60-blue-black.png" alt="mygserv.de" />
-                        </div>
-                    </div>
+                Vertreten durch:[Name des gesetzlichen Vertreters oder Verantwortlichen]
 
-                    <div class="lg:col-span-6 col-span-12 flex-col">
-                        <div class="">
-                            <textarea
-                                class="resize-none rounded-md w-full bg-secondary-50 text-gray-400"><a title="mygserv.de" href="https://mygserv.de"><img src="https://media.mygserv.de/banner/banner-468-60-blue-white.png" alt="mygserv.de" /></a></textarea>
-                        </div>
-                    </div>
-                    <div class="lg:col-span-6 col-span-12">
-                        <div class="flex justify-center items-center">
-                            <img class="inline-block align-middle mt-0 mb-0 center"
-                                src="https://media.mygserv.de/banner/banner-468-60-blue-white.png" alt="mygserv.de" />
-                        </div>
-                    </div>
+                Kontakt:Telefon: [Telefonnummer]E-Mail: [E-Mail-Adresse]Website: [Website-URL]
 
-                    <div class="lg:col-span-6 col-span-12 flex-col">
-                        <div class="">
-                            <textarea
-                                class="resize-none rounded-md w-full bg-secondary-50 text-gray-400"><a title="mygserv.de" href="https://mygserv.de"><img src="https://media.mygserv.de/banner/banner-88-31-blue-white.png" alt="mygserv.de" /></a></textarea>
-                        </div>
-                    </div>
-                    <div class="lg:col-span-6 col-span-12">
-                        <div class="flex justify-center items-center">
-                            <img class="inline-block align-middle mt-0 mb-0 center"
-                                src="https://media.mygserv.de/banner/banner-88-31-blue-white.png" alt="mygserv.de" />
-                        </div>
-                    </div>
+                Umsatzsteuer-ID:Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz: [falls zutreffend]
 
-                </div>
+                Haftung für Inhalte
+                Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den
+                allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+                verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu
+                forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung
+                der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine
+                diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung
+                möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend
+                entfernen.
+
+                Haftung für Links
+                Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben.
+                Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
+                verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die
+                verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+                Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
+                Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht
+                zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+
+                Urheberrecht
+                Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen
+                Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der
+                Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
+                Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+                Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte
+                Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem
+                auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
+                Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+
+                Streitbeilegung
+                Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
+                https://ec.europa.eu/consumers/odr. Unsere E-Mail-Adresse finden Sie oben im Impressum.
+
+                Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+                Verbraucherschlichtungsstelle teilzunehmen.
 
             </div>
         </div>
