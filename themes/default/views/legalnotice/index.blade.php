1c2553a13d693a4399ac8edb6519e120e9d4cfc2
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-14 02:52:33 +0100

    Update

--- themes/default/views/legalnotice/index.blade.php
+++ themes/default/views/legalnotice/index.blade.php
@@ -47,8 +47,12 @@
                     entfernen.') }}
                 </p>
 
-                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">Haftung für Links</h2>
-                <p>Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss
+                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
+                    {{ __('Haftung für Links') }}
+                </h2>
+                <p>
+                    {{ __('Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen
+                    Einfluss
                     haben.
                     Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der
                     verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die
@@ -56,11 +60,15 @@
                     Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente
                     inhaltliche
                     Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht
-                    zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+                    zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.')
+                    }}
                 </p>
 
-                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">Urheberrecht</h2>
-                <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
+                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
+                    {{ __('Urheberrecht') }}
+                </h2>
+                <p>
+                    {{ __('Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem
                     deutschen
                     Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb
                     der
@@ -71,14 +79,21 @@
                     Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie
                     trotzdem
                     auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
-                    Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+                    Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.') }}
+                </p>
 
-                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">Streitbeilegung</h2>
-                <p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:
-                    https://ec.europa.eu/consumers/odr.</p>
+                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
+                    {{ __('Streitbeilegung') }}
+                </h2>
+                <p>
+                    {{ __('Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:') }}
+                    https://ec.europa.eu/consumers/odr.
+                </p>
 
-                <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
-                    Verbraucherschlichtungsstelle teilzunehmen.</p>
+                <p>
+                    {{ __('Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+                    Verbraucherschlichtungsstelle teilzunehmen.') }}
+                </p>
             </div>
         </div>
     </div>
