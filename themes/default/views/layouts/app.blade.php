44b0ef6839328a0e7a0caac8a11fa92a3c55a12c
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-15 04:55:21 +0100

    Update

--- themes/default/views/layouts/app.blade.php
+++ themes/default/views/layouts/app.blade.php
@@ -46,60 +46,233 @@
         }
 
         :root {
-            --secondary-50: {{ config('settings::theme:secondary-50', '#ffffff') }};
-            --secondary-100: {{ config('settings::theme:secondary-100', '#fafcff') }};
-            --secondary-200: {{ config('settings::theme:secondary-200', '#ebeef3') }};
-            --secondary-300: {{ config('settings::theme:secondary-300', '#bbbfd2') }};
-            --secondary-400: {{ config('settings::theme:secondary-400', '#808498') }};
-            --secondary-500: {{ config('settings::theme:secondary-500', '#606372') }};
-            --secondary-600: {{ config('settings::theme:secondary-600', '#4d4f60') }};
-            --secondary-700: {{ config('settings::theme:secondary-700', '#353741') }};
-            --secondary-800: {{ config('settings::theme:secondary-800', '#1c1c20') }};
-            --secondary-900: {{ config('settings::theme:secondary-900', '#000000') }};
+            --secondary-50: {
+                    {
+                    config('settings::theme:secondary-50', '#ffffff')
+                }
+            }
 
-            --primary-50: {{ config('settings::theme:primary-50', '#EDF0FF') }};
-            --primary-100: {{ config('settings::theme:primary-100', '#C6DBFF') }};
-            --primary-200: {{ config('settings::theme:primary-200', '#9BBEFB') }};
-            --primary-300: {{ config('settings::theme:primary-300', '#799CD8') }};
-            --primary-400: {{ config('settings::theme:primary-400', '#5270FD') }};
+            ;
+
+            --secondary-100: {
+                    {
+                    config('settings::theme:secondary-100', '#fafcff')
+                }
+            }
+
+            ;
+
+            --secondary-200: {
+                    {
+                    config('settings::theme:secondary-200', '#ebeef3')
+                }
+            }
+
+            ;
+
+            --secondary-300: {
+                    {
+                    config('settings::theme:secondary-300', '#bbbfd2')
+                }
+            }
+
+            ;
+
+            --secondary-400: {
+                    {
+                    config('settings::theme:secondary-400', '#808498')
+                }
+            }
+
+            ;
+
+            --secondary-500: {
+                    {
+                    config('settings::theme:secondary-500', '#606372')
+                }
+            }
+
+            ;
+
+            --secondary-600: {
+                    {
+                    config('settings::theme:secondary-600', '#4d4f60')
+                }
+            }
+
+            ;
+
+            --secondary-700: {
+                    {
+                    config('settings::theme:secondary-700', '#353741')
+                }
+            }
+
+            ;
+
+            --secondary-800: {
+                    {
+                    config('settings::theme:secondary-800', '#1c1c20')
+                }
+            }
+
+            ;
+
+            --secondary-900: {
+                    {
+                    config('settings::theme:secondary-900', '#000000')
+                }
+            }
+
+            ;
+
+            --primary-50: {
+                    {
+                    config('settings::theme:primary-50', '#EDF0FF')
+                }
+            }
+
+            ;
+
+            --primary-100: {
+                    {
+                    config('settings::theme:primary-100', '#C6DBFF')
+                }
+            }
+
+            ;
+
+            --primary-200: {
+                    {
+                    config('settings::theme:primary-200', '#9BBEFB')
+                }
+            }
+
+            ;
+
+            --primary-300: {
+                    {
+                    config('settings::theme:primary-300', '#799CD8')
+                }
+            }
+
+            ;
+
+            --primary-400: {
+                    {
+                    config('settings::theme:primary-400', '#5270FD')
+                }
+            }
+
+            ;
         }
 
         .dark {
-            --secondary-50: {{ config('settings::theme:secondary-50-dark', '#1E202D') }};
-            --secondary-100: {{ config('settings::theme:secondary-100-dark', '#313441') }};
-            --secondary-200: {{ config('settings::theme:secondary-200-dark', '#404351') }};
-            --secondary-300: {{ config('settings::theme:secondary-300-dark', '#4F525E') }};
-            --secondary-400: {{ config('settings::theme:secondary-400-dark', '#656874') }};
-            --secondary-500: {{ config('settings::theme:secondary-500-dark', '#7D8091') }};
-            --secondary-600: {{ config('settings::theme:secondary-600-dark', '#AEB2C2') }};
-            --secondary-700: {{ config('settings::theme:secondary-700-dark', '#CACBD2') }};
-            --secondary-800: {{ config('settings::theme:secondary-800-dark', '#F1F1F1') }};
-            --secondary-900: {{ config('settings::theme:secondary-900-dark', '#ffffff') }};
+            --secondary-50: {
+                    {
+                    config('settings::theme:secondary-50-dark', '#1E202D')
+                }
+            }
+
+            ;
+
+            --secondary-100: {
+                    {
+                    config('settings::theme:secondary-100-dark', '#313441')
+                }
+            }
+
+            ;
+
+            --secondary-200: {
+                    {
+                    config('settings::theme:secondary-200-dark', '#404351')
+                }
+            }
+
+            ;
+
+            --secondary-300: {
+                    {
+                    config('settings::theme:secondary-300-dark', '#4F525E')
+                }
+            }
+
+            ;
+
+            --secondary-400: {
+                    {
+                    config('settings::theme:secondary-400-dark', '#656874')
+                }
+            }
+
+            ;
+
+            --secondary-500: {
+                    {
+                    config('settings::theme:secondary-500-dark', '#7D8091')
+                }
+            }
+
+            ;
+
+            --secondary-600: {
+                    {
+                    config('settings::theme:secondary-600-dark', '#AEB2C2')
+                }
+            }
+
+            ;
+
+            --secondary-700: {
+                    {
+                    config('settings::theme:secondary-700-dark', '#CACBD2')
+                }
+            }
+
+            ;
+
+            --secondary-800: {
+                    {
+                    config('settings::theme:secondary-800-dark', '#F1F1F1')
+                }
+            }
+
+            ;
+
+            --secondary-900: {
+                    {
+                    config('settings::theme:secondary-900-dark', '#ffffff')
+                }
+            }
+
+            ;
         }
     </style>
     @empty($title)
-        <title>{{ config('app.name', 'Paymenter') }}</title>
+    <title>{{ config('app.name', 'Paymenter') }}</title>
     @else
-        <title>{{ config('app.name', 'Paymenter') . ' - ' . ucfirst($title) }}</title>
+    <title>{{ config('app.name', 'Paymenter') . ' - ' . ucfirst($title) }}</title>
     @endempty
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&display=swap">
 
-    @vite(['themes/' . config('settings::theme-active') . '/js/app.js', 'themes/' . config('settings::theme-active') . '/css/app.css'], config('settings::theme-active'))
+    @vite(['themes/' . config('settings::theme-active') . '/js/app.js', 'themes/' . config('settings::theme-active') .
+    '/css/app.css'], config('settings::theme-active'))
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
     @if (config('settings::app_logo'))
-        <link rel="icon" href="{{ asset(config('settings::app_logo')) }}" type="image/png">
+    <link rel="icon" href="{{ asset(config('settings::app_logo')) }}" type="image/png">
     @else
-        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
     @endif
     <meta content="{{ ucfirst($title) ?? config('settings::seo_title') }}" property="og:title">
     <meta content="{{ $description ?? config('settings::seo_description') }}" property="og:description">
     <meta content="{{ $description ?? config('settings::seo_description') }}" name="description">
-    <meta content='{{ $image ?? config('settings::seo_image') }}' property='og:image'>
+    <meta content='{{ $image ?? config(' settings::seo_image') }}' property='og:image'>
     <link type="application/json+oembed"
         href="{{ url('/') }}/manifest.json?title={{ config('app.name', 'Paymenter') }}&author_url={{ url('/') }}&author_name={{ config('app.name', 'Paymenter') }}" />
     <meta name="twitter:card" content="@if (config('settings::seo_twitter_card')) summary_large_image @endif">
@@ -108,20 +281,31 @@
 
 <body class="font-sans bg-secondary-100 dark:bg-secondary-50 text-secondary-700">
 
+    <script type="module">
+        document.addEventListener("visibilitychange", () => {
+        if (document.hidden) {
+            document.title = "Hey, komm zurück!";
+        } else {
+            document.title = "Willkommen zurück!";
+        }
+        });
+    </script>
+
     @if (config('settings::theme:snow') == 1)
-        <canvas class="snow" id="snow" width="1920" height="1080"></canvas>
+    <canvas class="snow" id="snow" width="1920" height="1080"></canvas>
     @endif
     <div id="app" class="min-h-screen">
         <x-paymenter-update />
         @if (!$clients || config('settings::sidebar') == 0)
-            @include('layouts.navigation')
+        @include('layouts.navigation')
         @endif
         <div class="@if (config('settings::sidebar') == 1) flex md:flex-nowrap flex-wrap @endif">
             @if ($clients)
-                @include('layouts.subnavigation')
+            @include('layouts.subnavigation')
             @endif
-            <div class="w-full flex flex-col @if ($clients) min-h-[calc(100vh-105px)] @else min-h-[calc(100vh-64px)] @endif">
-                
+            <div
+                class="w-full flex flex-col @if ($clients) min-h-[calc(100vh-105px)] @else min-h-[calc(100vh-64px)] @endif">
+
                 <main class="grow">
                     {{ $slot }}
                 </main>
