85f8c8308e518f79a55f7f89606c31b57e0c09c9
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-20 00:09:33 +0100

    Update

--- themes/default/views/legalnotice/index.blade.php
+++ themes/default/views/legalnotice/index.blade.php
@@ -8,18 +8,12 @@
                 </header>
 
                 <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
-                    {{ __('Information according to § 5 TMG:') }}
+                    {{ __('Information according to § 5 DDG:') }}
                 </h2>
 
                 <p>Thorsten Schnack</p>
                 <p>Am Heidorn 7</p>
                 <p>30890 Barsinghausen</p>
-
-                <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
-                    {{ __('Represented by:') }}
-                </h2>
-
-                <p>Folgt</p>
 
                 <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
                     {{ __('Contact:') }}
@@ -33,14 +27,27 @@
                     {{ __('Liability for content') }}
                 </h2>
                 <p>
-                    {{ __('As a service provider, we are responsible for our own content on these pages in accordance with Section 7 Paragraph 1 TMG responsible for general laws. However, according to Sections 8 to 10 TMG, we are not a serviceprovider obliged to monitor transmitted or stored third-party information or according to circumstances to conduct research that indicates illegal activity. Obligations to remove or Blocking the use of information in accordance with general laws remains unaffected. However, liability in this regard only arises from the time of knowledge of a specific legal violation possible. If we become aware of any corresponding legal violations, we will immediately remove this content.') }}
+                    {{ __('As a service provider, we are responsible for our own content on these pages in accordance
+                    with Section 7 Paragraph 1 TMG responsible for general laws. However, according to Sections 8 to 10
+                    TMG, we are not a serviceprovider obliged to monitor transmitted or stored third-party information
+                    or according to circumstances to conduct research that indicates illegal activity. Obligations to
+                    remove or Blocking the use of information in accordance with general laws remains unaffected.
+                    However, liability in this regard only arises from the time of knowledge of a specific legal
+                    violation possible. If we become aware of any corresponding legal violations, we will immediately
+                    remove this content.') }}
                 </p>
 
                 <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
                     {{ __('Liability for links') }}
                 </h2>
                 <p>
-                    {{ __('Our offer contains links to external third-party websites over whose content we have no influence. We therefore cannot assume any liability for this external content. The respective provider or operator of the pages is always responsible for the content of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal content was not apparent at the time of linking. However, permanent control of the content of the linked pages is unreasonable without concrete evidence of a legal violation. If we become aware of any legal violations, we will immediately remove such links.')
+                    {{ __('Our offer contains links to external third-party websites over whose content we have no
+                    influence. We therefore cannot assume any liability for this external content. The respective
+                    provider or operator of the pages is always responsible for the content of the linked pages. The
+                    linked pages were checked for possible legal violations at the time of linking. Illegal content was
+                    not apparent at the time of linking. However, permanent control of the content of the linked pages
+                    is unreasonable without concrete evidence of a legal violation. If we become aware of any legal
+                    violations, we will immediately remove such links.')
                     }}
                 </p>
 
@@ -48,7 +55,14 @@
                     {{ __('Copyright') }}
                 </h2>
                 <p>
-                    {{ __('The content and works on these pages created by the site operators are subject to German copyright law. Reproduction, processing, distribution and any kind of exploitation outside the limits of copyright law require the written consent of the respective author or creator. Downloads and copies of this page are only permitted for private, non-commercial use. If the content on this site was not created by the operator, the copyrights of third parties are respected. In particular, third-party content is marked as such. Should you nevertheless become aware of a copyright infringement, we ask that you notify us accordingly. If we become aware of any legal violations, we will immediately remove such content.') }}
+                    {{ __('The content and works on these pages created by the site operators are subject to German
+                    copyright law. Reproduction, processing, distribution and any kind of exploitation outside the
+                    limits of copyright law require the written consent of the respective author or creator. Downloads
+                    and copies of this page are only permitted for private, non-commercial use. If the content on this
+                    site was not created by the operator, the copyrights of third parties are respected. In particular,
+                    third-party content is marked as such. Should you nevertheless become aware of a copyright
+                    infringement, we ask that you notify us accordingly. If we become aware of any legal violations, we
+                    will immediately remove such content.') }}
                 </p>
 
                 <h2 class="font-semibold text-xl mb-2 mt-2 text-secondary-900">
@@ -60,7 +74,8 @@
                 </p>
 
                 <p>
-                    {{ __('We are not willing or obliged to take part in dispute resolution proceedings before a consumer arbitration board.') }}
+                    {{ __('We are not willing or obliged to take part in dispute resolution proceedings before a
+                    consumer arbitration board.') }}
                 </p>
             </div>
         </div>
