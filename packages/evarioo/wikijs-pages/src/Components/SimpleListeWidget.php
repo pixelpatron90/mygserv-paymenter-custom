8558af6c3cd630e430a4ea6d76715b2a88aa9306
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-27 20:34:43 +0100

    Update

--- packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
+++ packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
@@ -13,6 +13,16 @@ class SimpleListeWidget extends Component
         //
     }
 
+    public function loadData()
+    {
+        $response = Http::withHeaders([
+            'X-API-Key' => $this->config('apiKey'),
+            'Content-Type' => 'application/json'
+        ])->get(config('evarioo-wikijs::wikijs.api_url'));
+
+        dd($response);
+    }
+
     public function render()
     {
         return view('evarioo-wikijs::widget.simple-list');
--- packages/evarioo/wikijs-pages/src/config.php
+++ packages/evarioo/wikijs-pages/src/config.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
+    'wikijs' => [
+        'api_url' => 'https://wiki.mygserv.de/graphql'
+    ],
     'simple-list-view' => null,
 ];
