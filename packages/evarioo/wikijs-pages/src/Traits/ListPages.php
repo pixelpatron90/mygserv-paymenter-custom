f7275798146ac4892a19ac0a5dc0f6a2d81ab947
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 14:45:53 +0100

    Update

---
+++ packages/evarioo/wikijs-pages/src/Traits/ListPages.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Evarioo\WikiJSPages\Traits;
+
+trait ListPages
+{
+    public $sortBy = '';
+    public $sortDirection = 'asc';
+
+    public function sortBy($field)
+    {
+        $this->sortDirection = $this->sortBy === $field
+            ? $this->reverseSort()
+            : 'asc';
+
+        $this->sortBy = $field;
+    }
+
+    public function reverseSort()
+    {
+        return $this->sortDirection === 'asc'
+            ? 'desc'
+            : 'asc';
+    }
+}
