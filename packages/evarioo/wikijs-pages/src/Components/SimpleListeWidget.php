c38f4065470ee4924451c10e93399e7da6885ad3
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 13:18:14 +0100

    Update

--- packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
+++ packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
@@ -20,17 +20,7 @@ class SimpleListeWidget extends Component
         $url = 'https://wiki.mygserv.de/graphql';
         $apiToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOjEsImdycCI6NSwiaWF0IjoxNzM4MDA2MTEzLCJleHAiOjE4MzI2Nzg5MTMsImF1ZCI6InVybjp3aWtpLmpzIiwiaXNzIjoidXJuOndpa2kuanMifQ.lnKS0UiauR7LylJIEK3gnV3xsN59nwDpXqAWD-WVckjh4R7TXD05sOGBmRuivmaUiLbWpEouw-P63yit1qN5DjF5p2-I-DpHvWUSHQpJmLCbrlIV9KT3jndle1ghj-tEgi_w3WYvKZ27P0a3wYkl2WvSqGUNCk9oMDWz_d6Wd7ovOLX5Ow2BxWua4eQwI-l4mQ8Fc-1TzCxeaKKpiiwVks_JWBw4FWp-9FabXAklcHVWzHsAbyH3en5D679tyKBh0vecbRLiLsAKjQ0So6B3IDw3pzbhiQaRpAz1_4uiOQOLcrIHK_5S6R4LCdgJuAYQIevqaBo9uqT4pQGp2GkZWA';
 
-        $query = <<<GRAPHQL
-        query {
-            pages {
-                id
-                path
-                title
-                createdAt
-                updatedAt
-            }
-        }
-        GRAPHQL;
+        $query = '{"query":"{\\r\\n  pages {\\r\\n    list (orderBy: TITLE) {\\r\\n      id\\r\\n      path\\r\\n      title\\r\\n    }\\r\\n  }\\r\\n}","variables":{}}';
 
         $result = Http::withToken($apiToken)
             ->post($url, [
