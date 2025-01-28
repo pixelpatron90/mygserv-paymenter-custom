3a8b76703cc4ee8779566feb3c3f78aa8a19387e
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 14:02:54 +0100

    Update

--- packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
+++ packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
@@ -2,9 +2,6 @@
 
 namespace Evarioo\WikiJSPages\Components;
 
-use config;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
@@ -19,11 +16,9 @@ class SimpleListeWidget extends Component
     public function loadData()
     {
 
-        // GraphQL-API URL
         $apiUrl = 'https://wiki.mygserv.de/graphql';
         $apiToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOjEsImdycCI6NSwiaWF0IjoxNzM4MDA2MTEzLCJleHAiOjE4MzI2Nzg5MTMsImF1ZCI6InVybjp3aWtpLmpzIiwiaXNzIjoidXJuOndpa2kuanMifQ.lnKS0UiauR7LylJIEK3gnV3xsN59nwDpXqAWD-WVckjh4R7TXD05sOGBmRuivmaUiLbWpEouw-P63yit1qN5DjF5p2-I-DpHvWUSHQpJmLCbrlIV9KT3jndle1ghj-tEgi_w3WYvKZ27P0a3wYkl2WvSqGUNCk9oMDWz_d6Wd7ovOLX5Ow2BxWua4eQwI-l4mQ8Fc-1TzCxeaKKpiiwVks_JWBw4FWp-9FabXAklcHVWzHsAbyH3en5D679tyKBh0vecbRLiLsAKjQ0So6B3IDw3pzbhiQaRpAz1_4uiOQOLcrIHK_5S6R4LCdgJuAYQIevqaBo9uqT4pQGp2GkZWA';
 
-        // Headers für die Anfrage
         $headers = [
             'Content-Type' => 'application/json',
             'Authorization' => 'Bearer ' . $apiToken,
@@ -67,6 +62,9 @@ class SimpleListeWidget extends Component
     public function render()
     {
         dd($this->loadData());
+        return view('evarioo-wikijs::widget.simple-list', [
+            'data' => $this->loadData()
+        ])
     }
 
 }
