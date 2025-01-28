450489d16343904091b2442641772492ae825695
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 12:24:26 +0100

    Update

--- packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
+++ packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
@@ -17,14 +17,10 @@ class SimpleListeWidget extends Component
     public function loadData()
     {
 
-        $query = '{"query": "query { viewer { repositories(last: 100) { nodes { name id isPrivate nameWithOwner } } } } "}';
-
         $response = Http::withHeaders([
             'X-API-Key' => config('evarioo-wikijs.wikijs.api_key'),
             'Content-Type' => 'application/json'
-        ])->get(config('evarioo-wikijs.wikijs.api_url'), [
-                    'query' => $query
-                ]);
+        ])->get(config('evarioo-wikijs.wikijs.api_url' . '/api/v1/pages'));
 
         dd($response->json());
     }
