bb1221dba7112d37ca94762eee2c54ea9c53464a
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 13:35:42 +0100

    Update

--- packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
+++ packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
@@ -56,9 +56,58 @@ class SimpleListeWidget extends Component
 
     }
 
+    public function loadData2()
+    {
+
+        // GraphQL-API URL
+        $url = 'https://wiki.mygserv.de/graphql';
+
+        // Headers für die Anfrage
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGkiOjEsImdycCI6NSwiaWF0IjoxNzM4MDA2MTEzLCJleHAiOjE4MzI2Nzg5MTMsImF1ZCI6InVybjp3aWtpLmpzIiwiaXNzIjoidXJuOndpa2kuanMifQ.lnKS0UiauR7LylJIEK3gnV3xsN59nwDpXqAWD-WVckjh4R7TXD05sOGBmRuivmaUiLbWpEouw-P63yit1qN5DjF5p2-I-DpHvWUSHQpJmLCbrlIV9KT3jndle1ghj-tEgi_w3WYvKZ27P0a3wYkl2WvSqGUNCk9oMDWz_d6Wd7ovOLX5Ow2BxWua4eQwI-l4mQ8Fc-1TzCxeaKKpiiwVks_JWBw4FWp-9FabXAklcHVWzHsAbyH3en5D679tyKBh0vecbRLiLsAKjQ0So6B3IDw3pzbhiQaRpAz1_4uiOQOLcrIHK_5S6R4LCdgJuAYQIevqaBo9uqT4pQGp2GkZWA',
+        ];
+
+        // GraphQL-Query
+        $query = <<<'GRAPHQL'
+        {
+          pages {
+            list(orderBy: TITLE) {
+              id
+              path
+              title
+            }
+          }
+        }
+        GRAPHQL;
+
+        // Body der Anfrage
+        $body = [
+            'query' => $query,
+            'variables' => new \stdClass(), // Leere Variablen
+        ];
+
+        // Sende POST-Anfrage
+        $response = Http::withHeaders($headers)->post($url, $body);
+
+        // Überprüfen, ob die Anfrage erfolgreich war
+        if ($response->successful()) {
+            // JSON-Ergebnis zurückgeben
+            return response()->json($response->json());
+        } else {
+            // Fehlerbehandlung
+            return response()->json([
+                'error' => 'GraphQL request failed',
+                'status' => $response->status(),
+                'message' => $response->body(),
+            ], $response->status());
+        }
+
+    }
+
     public function render()
     {
-        dd($this->loadData());
+        dd($this->loadData2());
     }
 
 }
