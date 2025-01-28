6dbb538ed5bf51caebc920e90356fcc1c3e58ed5
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 18:28:42 +0100

    Update

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -7,7 +7,6 @@ class MediaController extends Controller
 {
     public function index()
     {
-        dd(session()->get('locale'));
-        //return view('media.index');
+        return view('media.index');
     }
 }
