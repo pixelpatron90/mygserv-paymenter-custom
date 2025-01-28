1bedb645f81ffe9ca867051f6a0d720a338cb77a
Alexander Bergmann <alxndr.wlfgng.brgmnn@gmail.com>
2025-01-28 13:53:29 +0100

    Update

--- packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
+++ packages/evarioo/wikijs-pages/src/Components/SimpleListeWidget.php
@@ -56,7 +56,7 @@ class SimpleListeWidget extends Component
             return $widgetdata;
         }
 
-        dd($response->json()['data']['pages']['list']['title']);
+        dd($response->json()['data']['pages']['list']->title);
         //return response()->json($response->json());
 
         $widgetdata = (object) [
